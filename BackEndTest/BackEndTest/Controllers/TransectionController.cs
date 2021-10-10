c63071d75ebd6879a0283a4dddb6a1d37d54cc75
hady <hadysas@gmail.com>
2021-10-10 20:39:13 +0700

    [DEL] remove unuse code

---
+++ BackEndTest/BackEndTest/Controllers/TransectionController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEndTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TransectionController : Controller
+    {
+        private readonly ILogger<TransectionController> _logger;
+        public TransectionController(ILogger<TransectionController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
