d091fc2646ee947146ab8a11318ae2ff7dc97fc3
hady <hadysas@gmail.com>
2021-10-10 20:29:51 +0700

    [ADD] DB migration

---
+++ BackEndTest/BackEndTest/Migrations/20211010132533_initial_migration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BackEndTest.Migrations
+{
+    public partial class initial_migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Transaction",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CurrenctCode = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: false),
+                    TransectionDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transaction", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transaction");
+        }
+    }
+}
--- BackEndTest/BackEndTest/Startup.cs
+++ BackEndTest/BackEndTest/Startup.cs
@@ -1,6 +1,8 @@
+using BackEndTest.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,7 +26,9 @@ namespace BackEndTest
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(
+                Configuration.GetConnectionString("Dafault")));
+               services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
